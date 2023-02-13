a628f854bdc6a92443527f1c1b3ec77f160a7f60
Torsten Grote <t@grobox.de>
2023-02-13 15:19:27 -0300

    Add basic updater app
    Change-Id: I75b32b5225a323680d3afabec1f48cda6c5ec6ce

---
+++ updater/src/main/java/org/calyxos/lupin/updater/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: Copyright 2022 The Calyx Institute
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.calyxos.lupin.updater.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val SystemAccent1_500 = Color(0xFF6c7d30)
+val SystemAccent1_600 = Color(0xFF546519)
---
+++ updater/src/main/java/org/calyxos/lupin/updater/ui/theme/Theme.kt
@@ -0,0 +1,30 @@
+/*
+ * SPDX-FileCopyrightText: Copyright 2022 The Calyx Institute
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.calyxos.lupin.updater.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+fun LupinTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        dynamicDarkColorScheme(LocalContext.current)
+    } else {
+        dynamicLightColorScheme(LocalContext.current)
+    }
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
