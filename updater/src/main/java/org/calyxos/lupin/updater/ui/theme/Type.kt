2fc55bc58e5dc0e721e57e4c59c8f2c43853d7d9
Torsten Grote <t@grobox.de>
2023-02-13 15:19:27 -0300

    Add basic updater app
    Change-Id: I75b32b5225a323680d3afabec1f48cda6c5ec6ce

---
+++ updater/src/main/java/org/calyxos/lupin/updater/ui/theme/Type.kt
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: Copyright 2022 The Calyx Institute
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.calyxos.lupin.updater.ui.theme
+
+import androidx.compose.material3.Typography
+
+// Set of Material typography styles to start with
+val Typography = Typography()
