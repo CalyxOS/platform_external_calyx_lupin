3233c2baf795dcf15b53ab232f9f23e989da6f9d
Torsten Grote <t@grobox.de>
2023-02-17 16:48:07 -0300

    Move update logic into Worker
    and use WorkManager API to start it. The UI also disables the 'Check now' button while the worker is running.
    
    The lastCheck state had to be moved into the SettingsManager, so the worker could access it.
    
    While adding new dependencies, the lifecycle-service dependency was moved back into the installer as the updater doesn't need it.
    
    Change-Id: Ib17bbee7f0efe43bfe1f34cbd60eac779a51e0e5

--- updater/src/main/java/org/calyxos/lupin/updater/MainActivity.kt
+++ updater/src/main/java/org/calyxos/lupin/updater/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import dagger.hilt.android.AndroidEntryPoint
@@ -57,9 +58,11 @@ class MainActivity : ComponentActivity() {
                         },
                     ) {
                         val lastCheckedMillis = viewModel.lastCheckedMillis.collectAsState()
+                        val enabled = viewModel.checkLiveData.observeAsState()
                         StatusScreen(
                             paddingValues = it,
                             lastCheckedMillis = lastCheckedMillis.value,
+                            checkButtonEnabled = enabled.value ?: true,
                             onCheckButtonClicked = viewModel::onCheckButtonClicked,
                         )
                     }
