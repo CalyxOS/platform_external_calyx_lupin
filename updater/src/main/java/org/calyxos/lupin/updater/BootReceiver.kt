04fe7e11a4f46d39197cac096664581d7178ca00
Torsten Grote <t@grobox.de>
2023-02-17 17:10:41 -0300

    Schedule the UpdateWorker via a BOOT_RECEIVER to run periodically
    To work reliably, this might need further measures such as making the app a system app or 'allow-in-power-save'.
    
    Change-Id: Ifa4110ad2d7cfc64606c530575cbb0166f058fa1

---
+++ updater/src/main/java/org/calyxos/lupin/updater/BootReceiver.kt
@@ -0,0 +1,54 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.calyxos.lupin.updater
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.ACTION_BOOT_COMPLETED
+import androidx.work.BackoffPolicy.EXPONENTIAL
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy.KEEP
+import androidx.work.NetworkType.UNMETERED
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.WorkRequest.Companion.DEFAULT_BACKOFF_DELAY_MILLIS
+import mu.KotlinLogging
+import java.util.concurrent.TimeUnit.HOURS
+import java.util.concurrent.TimeUnit.MILLISECONDS
+import java.util.concurrent.TimeUnit.MINUTES
+
+class BootReceiver : BroadcastReceiver() {
+
+    private val log = KotlinLogging.logger {}
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action != ACTION_BOOT_COMPLETED) return
+
+        log.info { "Scheduling periodic update worker" }
+
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .build()
+        val workRequest = PeriodicWorkRequestBuilder<UpdateWorker>(
+            repeatInterval = 3,
+            repeatIntervalTimeUnit = HOURS,
+            flexTimeInterval = 30,
+            flexTimeIntervalUnit = MINUTES
+        ).setBackoffCriteria(
+            backoffPolicy = EXPONENTIAL,
+            backoffDelay = DEFAULT_BACKOFF_DELAY_MILLIS,
+            timeUnit = MILLISECONDS,
+        ).setInitialDelay(
+            duration = 10,
+            timeUnit = MINUTES,
+        ).setConstraints(constraints).build()
+
+        val workManager = WorkManager.getInstance(context)
+        workManager.enqueueUniquePeriodicWork(WORK_NAME_PERIODIC, KEEP, workRequest)
+    }
+}
