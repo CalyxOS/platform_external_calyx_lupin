298b57898ac7dd19d45f9815fa017182885c6bac
Chirayu Desai <chirayudesai1@gmail.com>
2023-02-24 05:28:59 +0530

    installer: Point REPO_URL to a non-existent repo until we're ready
    * Just being a bit paranoid
    * If this tries to access said URL, it'll get a 404
    * It's still our server and already used by F-Droid so it
      won't be a new connection at least
    
    Change-Id: I0af95a6952e80867b3cfcae916ac752a64b51206

--- installer/src/main/java/org/calyxos/lupin/installer/state/RepoManager.kt
+++ installer/src/main/java/org/calyxos/lupin/installer/state/RepoManager.kt
@@ -29,7 +29,7 @@ import javax.inject.Singleton
 
 internal const val REPO_INDEX = "index-v1.jar"
 internal const val REPO_PATH = "/product/fdroid/repo"
-internal const val REPO_URL = "https://calyxos.gitlab.io/calyx-fdroid-repo/fdroid/repo"
+internal const val REPO_URL = "https://fdroid-repo.calyxinstitute.org/non/existent/repo"
 internal const val CATEGORY_DEFAULT = "Default"
 internal const val CATEGORY_ONLINE_ONLY = "Internet"
 
