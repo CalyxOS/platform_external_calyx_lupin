ff552b75c761989c0579aa48c41e9a80d919ecb1
Torsten Grote <t@grobox.de>
2023-02-24 14:18:53 -0300

    Introduce our own NotificationManager
    to handle ongoing update check notification and future user confirmation notification.
    
    Change-Id: I91b0cd2615c71e370881dae38d39fd91e0377243

--- updater/src/main/java/org/calyxos/lupin/updater/UpdateWorker.kt
+++ updater/src/main/java/org/calyxos/lupin/updater/UpdateWorker.kt
@@ -5,16 +5,10 @@
 
 package org.calyxos.lupin.updater
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.NotificationManager.IMPORTANCE_LOW
 import android.content.Context
-import android.content.Context.NOTIFICATION_SERVICE
-import androidx.core.app.NotificationCompat
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.ForegroundInfo
-import androidx.work.WorkManager
 import androidx.work.WorkerParameters
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
@@ -22,8 +16,6 @@ import mu.KotlinLogging
 
 const val WORK_NAME_MANUAL = "manual"
 const val WORK_NAME_PERIODIC = "periodic"
-private const val CHANNEL_ID = "updateNotification"
-private const val NOTIFICATION_ID = 1
 
 @HiltWorker
 class UpdateWorker @AssistedInject constructor(
@@ -31,11 +23,10 @@ class UpdateWorker @AssistedInject constructor(
     @Assisted workerParams: WorkerParameters,
     private val settingsManager: SettingsManager,
     private val updateManager: UpdateManager,
+    private val notificationManager: NotificationManager,
 ) : CoroutineWorker(context, workerParams) {
 
     private val log = KotlinLogging.logger {}
-    private val notificationManager =
-        context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
 
     override suspend fun doWork(): Result {
         try {
@@ -62,28 +53,8 @@ class UpdateWorker @AssistedInject constructor(
     }
 
     private fun createForegroundInfo(): ForegroundInfo {
-        val title = applicationContext.getString(R.string.notification_title)
-        val cancel = applicationContext.getString(R.string.cancel)
-        // This PendingIntent can be used to cancel the worker
-        val intent = WorkManager.getInstance(applicationContext)
-            .createCancelPendingIntent(id)
-        createChannel()
-        val notification = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
-            .setContentTitle(title)
-            .setTicker(title)
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setOngoing(true)
-            .addAction(android.R.drawable.ic_delete, cancel, intent)
-            .build()
-        return ForegroundInfo(NOTIFICATION_ID, notification)
-    }
-
-    private fun createChannel() {
-        val name = applicationContext.getString(R.string.channel_name)
-        val channel = NotificationChannel(CHANNEL_ID, name, IMPORTANCE_LOW).apply {
-            description = applicationContext.getString(R.string.channel_description)
-        }
-        notificationManager.createNotificationChannel(channel)
+        val notification = notificationManager.getUpdateNotification(id)
+        return ForegroundInfo(NOTIFICATION_UPDATE_ID, notification)
     }
 
 }
