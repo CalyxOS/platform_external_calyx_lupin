43b617a011c2a981350b1848c83314f56468eb24
Oliver Scott <olivercscott@gmail.com>
2023-04-26 16:16:56 -0400

    Migrate demoted system apps
    Create a self-disabling boot receiver that installs demoted system apps
    
    Change-Id: Ie82ede824fe904ba419933bdc560331527f38c4d

---
+++ installer/src/main/java/org/calyxos/lupin/installer/install/BootReceiver.kt
@@ -0,0 +1,53 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.calyxos.lupin.installer.install
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.work.CoroutineWorker
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.OutOfQuotaPolicy
+import androidx.work.WorkManager
+import androidx.work.WorkerParameters
+import org.calyxos.lupin.installer.PackageInstaller
+import org.calyxos.lupin.installer.install.AppInstaller.Companion.setInstallerPackageName
+import org.calyxos.lupin.installer.state.REPO_PATH
+import java.io.File
+
+
+class BootReceiver : BroadcastReceiver() {
+    companion object {
+        val DEMOTED_SYSTEM_APPS = mapOf(Pair("org.fdroid.fdroid", "F-Droid.apk"))
+    }
+
+    class SystemAppDemotionWorker(context: Context, workerParameters: WorkerParameters) :
+        CoroutineWorker(context, workerParameters) {
+        override suspend fun doWork(): Result {
+            DEMOTED_SYSTEM_APPS.forEach {
+                if (PackageInstaller(applicationContext.applicationContext).install(
+                        it.key, File(REPO_PATH, it.value)
+                    ) { setInstallerPackageName(INSTALLER_PACKAGE_NAME) }.success
+                ) {
+                    return Result.retry()
+                }
+            }
+            return Result.success()
+        }
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        when (intent.action) {
+            Intent.ACTION_BOOT_COMPLETED -> {
+                WorkManager.getInstance(context).enqueue(
+                    OneTimeWorkRequestBuilder<SystemAppDemotionWorker>().setExpedited(
+                        OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST
+                    ).build()
+                )
+            }
+        }
+    }
+}
