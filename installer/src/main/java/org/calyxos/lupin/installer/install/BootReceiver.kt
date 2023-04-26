c8c399a5a60043a9e94ca9aa9977221033c5a741
Oliver Scott <olivercscott@gmail.com>
2023-04-26 16:16:56 -0400

    Migrate demoted system apps
    Create a boot receiver that installs demoted system apps
    
    Change-Id: Ie82ede824fe904ba419933bdc560331527f38c4d

---
+++ installer/src/main/java/org/calyxos/lupin/installer/install/BootReceiver.kt
@@ -0,0 +1,62 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.calyxos.lupin.installer.install
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.work.CoroutineWorker
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.OutOfQuotaPolicy
+import androidx.work.WorkManager
+import androidx.work.WorkerParameters
+import org.calyxos.lupin.ApkInstaller
+import org.calyxos.lupin.installer.install.AppInstaller.Companion.setInstallerPackageName
+import org.calyxos.lupin.installer.state.REPO_PATH
+import java.io.File
+
+class BootReceiver : BroadcastReceiver() {
+    companion object {
+        val DEMOTED_SYSTEM_APPS = mapOf(Pair("org.fdroid.fdroid", "F-Droid.apk"))
+    }
+
+    class SystemAppDemotionWorker(context: Context, workerParameters: WorkerParameters) :
+        CoroutineWorker(context, workerParameters) {
+        override suspend fun doWork(): Result {
+            val apkInstaller = ApkInstaller(applicationContext)
+            DEMOTED_SYSTEM_APPS.forEach {
+                try {
+                    applicationContext.packageManager.getPackageInfo(
+                        it.key,
+                        PackageManager.PackageInfoFlags.of(0.toLong())
+                    )
+                } catch (e: PackageManager.NameNotFoundException) {
+                    if (!apkInstaller.install(
+                            it.key,
+                            File(REPO_PATH, it.value)
+                        ) { setInstallerPackageName(INSTALLER_PACKAGE_NAME) }.success
+                    ) {
+                        return Result.failure()
+                    }
+                }
+            }
+            return Result.success()
+        }
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        when (intent.action) {
+            Intent.ACTION_BOOT_COMPLETED -> {
+                WorkManager.getInstance(context).enqueue(
+                    OneTimeWorkRequestBuilder<SystemAppDemotionWorker>().setExpedited(
+                        OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST
+                    ).build()
+                )
+            }
+        }
+    }
+}
