5c86ae3657b5610d0fd6dfd865c7d9d5939e0b38
Chirayu Desai <chirayudesai1@gmail.com>
2023-09-09 02:58:31 +0530

    installer: Introduce category AlwaysInstall
    * Used for apps such as F-Droid, which we include this
      way but want to install always.
    * Don't need to show them in the UI either, but we
      want to make sure that they're always installed
    
    Change-Id: Ib064eabcaa9e880518f4455dae98964fce36d2b4

--- installer/src/main/java/org/calyxos/lupin/installer/state/RepoManager.kt
+++ installer/src/main/java/org/calyxos/lupin/installer/state/RepoManager.kt
@@ -27,6 +27,7 @@ import javax.inject.Singleton
 internal const val REPO_INDEX = "index-v1.jar"
 internal const val REPO_PATH = "/product/fdroid/repo"
 internal const val REPO_URL = "https://fdroid-repo.calyxinstitute.org/non/existent/repo"
+internal const val CATEGORY_ALWAYS_INSTALL = "AlwaysInstall"
 internal const val CATEGORY_DEFAULT = "Default"
 internal const val CATEGORY_ONLINE_ONLY = "OnlineOnly"
 
