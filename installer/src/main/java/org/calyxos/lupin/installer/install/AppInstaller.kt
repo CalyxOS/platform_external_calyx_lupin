910df4b018b189bafd06b6da9daeb6117ffa9b5a
Chirayu Desai <chirayudesai1@gmail.com>
2023-10-10 04:50:55 +0530

    Revert "Migrate demoted system apps"
    This reverts commit 308b2bc7ee67beedb71bc6fbfa697dcf94abcc72.
    
    Reason: No longer needed, migration has been done
    Change-Id: I251a0c9347dc1b9a7d29ac8220c98345786df5d2

--- installer/src/main/java/org/calyxos/lupin/installer/install/AppInstaller.kt
+++ installer/src/main/java/org/calyxos/lupin/installer/install/AppInstaller.kt
@@ -29,7 +29,7 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 private const val TAG = "AppInstaller"
-const val INSTALLER_PACKAGE_NAME = "org.fdroid.fdroid.privileged"
+private const val INSTALLER_PACKAGE_NAME = "org.fdroid.fdroid.privileged"
 
 @Singleton
 class AppInstaller @Inject constructor(
@@ -43,13 +43,6 @@ class AppInstaller @Inject constructor(
      * This provides updated [UiState] only after calling [installApps].
      */
     val uiState = _uiState.asStateFlow()
-
-    companion object {
-        fun SessionParams.setInstallerPackageName(packageName: String) {
-            val method = javaClass.methods.find { it.name == "setInstallerPackageName" }
-            method?.invoke(this, packageName)
-        }
-    }
 
     internal suspend fun installApps(items: MutableList<AppItem>) {
         _uiState.value = UiState.SelectionComplete(items)
@@ -145,4 +138,10 @@ class AppInstaller @Inject constructor(
             set(index, item)
         }
     }
+
+    private fun SessionParams.setInstallerPackageName(packageName: String) {
+        val method = javaClass.methods.find { it.name == "setInstallerPackageName" }
+        method?.invoke(this, packageName)
+    }
+
 }
