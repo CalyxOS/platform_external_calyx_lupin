2fb165ed7948dfd6a440523e2cf3134c314210b5
Michael Bestas <mikeioannina@gmail.com>
2024-02-28 16:46:30 +0200

    Update theme for U design
    Change-Id: I57f33148ec80f969569864f7d2c43c8373de4e2c
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- installer/src/main/java/org/calyxos/lupin/installer/ui/theme/Theme.kt
+++ installer/src/main/java/org/calyxos/lupin/installer/ui/theme/Theme.kt
@@ -8,16 +8,21 @@ package org.calyxos.lupin.installer.ui.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import org.calyxos.lupin.installer.R
 
 @Composable
 fun LupinTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         dynamicDarkColorScheme(LocalContext.current)
     } else {
-        dynamicLightColorScheme(LocalContext.current)
+        lightColorScheme(
+            primary = colorResource(id = R.color.accent),
+            background = colorResource(id = R.color.primary)
+        )
     }
 
     MaterialTheme(
