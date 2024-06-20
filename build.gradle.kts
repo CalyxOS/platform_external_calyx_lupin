e21db98107c167ed67946ee37fdfb1b7eb6830ae
Aayush Gupta <aayushgupta219@gmail.com>
2024-06-20 12:38:09 +0530

    Bump to latest stable AGP, Gradle and dependencies
    Change-Id: Iab4090a696baed1ada912c0ee845ba88c89e4a25
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,14 +6,14 @@
 buildscript {
     extra.apply {
         set("sdk", 34)
-        set("compose_compiler_version", "1.5.12")
     }
 }
 
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.jetbrains.kotlin.compose) apply false
     alias(libs.plugins.googleKsp) apply false
     alias(libs.plugins.googleHilt) apply false
     alias(libs.plugins.ktlint) apply false
